2d32107a13ed26e7d76c834cca9296ab84a8dd6f
AngeviLee <568178364@qq.com>
2020-02-29 14:09:01 +0800

    子组件switch.vue添加基本的结构和样式

--- lyd-ui/src/components/switch.vue
+++ lyd-ui/src/components/switch.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>123</div>
+  <label class="lyd-switch">
+    <span class="lyd-switch_core">
+      <span class="lyd-switch_button"></span>
+    </span>
+  </label>
 </template>
 
 <script>
@@ -8,6 +12,39 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="scss">
+  .lyd-switch {
+    display: inline-flex;
+    align-items: center;
+    position: relative;
+    font-size: 14px;
+    line-height: 20px;
+    height: 20px;
+    vertical-align: middle;
+    .lyd-switch_core {
+      margin: 0;
+      display: inline-block;
+      position: relative;
+      width: 40px;
+      height: 20px;
+      border: 1px solid #dcdfe6;
+      outline: none;
+      border-radius: 10px;
+      box-sizing: border-box;
+      background: #dcdfe6;
+      cursor: pointer;
+      transition: border-color .3s,background-color .3s;
+      vertical-align: middle;
+      .lyd-switch_button {
+        position: absolute;
+        top: 1px;
+        left: 1px;
+        border-radius: 100%;
+        transition: all .3s;
+        width: 16px;
+        height: 16px;
+        background-color: #fff;
+      }
+    }
+  }
 </style>
